1192107189c6f022d0f49b5572073d6c95cb81df
Alice <ann.bondarets22@gmail.com>
2023-07-14 08:54:25 -0500

    Created components

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainBanner from '../main-banner/MainBanner'
+import MainCards from '../main-cards/MainCards'
+
+const Main = () => {
+  return (
+    <main>
+        <MainBanner/>
+        <MainCards/>
+    </main>
+  )
+}
+
+export default Main
